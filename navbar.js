bcd022d7904662f57c93fb2368895a07b8dcaae6
Marion Séminel <marionement@gmail.com>
2019-11-18 20:26:33 +0100

    project 3 completed but not verified

--- navbar.js
+++ navbar.js
@@ -1,17 +1,35 @@
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
+let highlight = document.getElementsByClassName('highlight');
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+
+
+const makeAppear = (x) => {
+  if ( x == null ) return;
+  if (window.pageYOffset >= x.offsetTop-400) {
+    x.style.backgroundPosition="-100% 0";
+    return;
+  }
+  else {
+    x.style.backgroundPosition="0 0";
+  }
+}
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  if (navbar != null) {
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+  for (let item of highlight) {
+    makeAppear(item)
   }
 }
